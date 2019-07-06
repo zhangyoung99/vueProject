d4548624d454068f782ef9c93059102c2c92305a
zhangyoung99 <zhangyoung11@gmail.com>
2019-07-06 15:38:22 +0800

    router config

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="layout">
+    <Header />
+    <SideMenu />
+    <Footer />
+    <router-view></router-view>
+</div>
+    
+</template>
+
+<script>
+import Header from "./header.vue";
+import SideMenu from "./sideMenu.vue";
+import Footer from "./footer.vue";
+
+export default {
+    data(){
+        return {
+            collapsed: false,
+        }
+    },
+    components: {
+        Header,
+        SideMenu,
+        Footer
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/layouts/UserLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h3 class="title">Ant design vue pro</h3>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
